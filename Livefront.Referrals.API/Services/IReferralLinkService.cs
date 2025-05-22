1efffe6fe2c0f2c4a4e281d9a77d31c541d3b750
Stephon <stephonb14@gmail.com>
2025-05-22 13:29:52 -0400

    fix: add cancellationtokens to referrallink service functions

--- Livefront.Referrals.API/Services/IReferralLinkService.cs
+++ Livefront.Referrals.API/Services/IReferralLinkService.cs
@@ -11,13 +11,15 @@ public interface IReferralLinkService
     /// Creates a new referral link for a user or retrieves an existing one if it already exists.
     /// </summary>
     /// <param name="userId">The unique id of the user.</param>
+    /// <param name="cancellationToken"></param>
     /// <returns>A <see cref="Task"/> that represents the async operation, containing the <see cref="ReferralLinkDTO"/>.</returns>
-    Task<ReferralLinkDTO> CreateOrGetReferralLink(Guid userId);
+    Task<ReferralLinkDTO> CreateOrGetReferralLink(Guid userId, CancellationToken cancellationToken);
 
     /// <summary>
     /// Extends the time-to-live (TTL) of a referral link for a specific user.
     /// </summary>
     /// <param name="userId">The unique id of the user.</param>
+    /// <param name="cancellationToken"></param>
     /// <returns>A <see cref="Task"/> that represents the async operation, containing the updated <see cref="ReferralLinkDTO"/>.</returns>
-    Task<ReferralLinkDTO> ExtendReferralLinkTimeToLive(Guid userId);
+    Task<ReferralLinkDTO> ExtendReferralLinkTimeToLive(Guid userId, CancellationToken cancellationToken);
 }
