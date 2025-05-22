6de560bffcfd7803bc958d076e2eebe56cb6776c
Stephon <stephonb14@gmail.com>
2025-05-22 15:39:19 -0400

    feat: add deeplink conversion to extensions

--- Livefront.Referrals.API/Extensions/ReferralLinkExtensions.cs
+++ Livefront.Referrals.API/Extensions/ReferralLinkExtensions.cs
@@ -1,15 +1,27 @@
 using Livefront.Referrals.API.Models;
 using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Models.DeeplinkApi.Models;
 
 namespace Livefront.Referrals.API.Extensions;
 
 public static class ReferralLinkExtensions
 {
-    public static ReferralLinkDTO ReferralLinkToReferralLinkDto(this ReferralLink referralLink)
+    public static ReferralLinkDTO ToReferralLinkDto(this ReferralLink referralLink)
     {
         return new ReferralLinkDTO()
         {
             ReferralLink = referralLink.BaseDeepLink,
+            ExpirationDate = referralLink.ExpirationDate,
+        };
+    }
+    
+    public static DeepLink ToDeepLink(this ReferralLink referralLink)
+    {
+        return new DeepLink
+        {
+            Id = referralLink.ThirdPartyId,
+            Link = referralLink.BaseDeepLink,
+            DateCreated = referralLink.DateCreated,
             ExpirationDate = referralLink.ExpirationDate,
         };
     }
