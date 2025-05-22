2c752fb76ce69c0e94fcd5998273f6dfceee079a
Stephon <stephonb14@gmail.com>
2025-05-22 13:27:01 -0400

    feat: add UserNotFoundException

---
+++ Livefront.Referrals.API/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,6 @@
+namespace Livefront.Referrals.API.Exceptions;
+
+public class UserNotFoundException(Guid userId) : Exception($"User {userId} was not found")
+{
+    public Guid UserId { get; private set; }
+}
