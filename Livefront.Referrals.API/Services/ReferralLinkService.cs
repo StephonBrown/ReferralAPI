e7054d5c0af3e2b7e1048699db97d02ad96af1f4
Stephon <stephonb14@gmail.com>
2025-05-22 13:30:27 -0400

    feat: implement CreateOrGetReferralLink function

--- Livefront.Referrals.API/Services/ReferralLinkService.cs
+++ Livefront.Referrals.API/Services/ReferralLinkService.cs
@@ -1,33 +1,71 @@
+using Livefront.Referrals.API.Exceptions;
+using Livefront.Referrals.API.Extensions;
 using Livefront.Referrals.API.Models;
+using Livefront.Referrals.DataAccess.Models;
 using Livefront.Referrals.DataAccess.Repositories;
 using Livefront.Referrals.DataAccess.Services;
 
 namespace Livefront.Referrals.API.Services;
 
-
 public class ReferralLinkService : IReferralLinkService
 {
     private readonly IReferralLinkRepository referralLinkRepository;
-    private readonly ExternalDeeplinkApiService externalDeeplinkApiService;
-    private readonly IUserRespository userRespository;
+    private readonly IExternalDeeplinkApiService externalDeeplinkApiService;
+    private readonly IUserRepository userRepository;
+    private readonly ILogger<IReferralLinkService> logger;
     
     public ReferralLinkService(IReferralLinkRepository referralLinkRepository, 
-        IUserRespository userRespository, 
-        ExternalDeeplinkApiService externalDeeplinkApiService)
+        IUserRepository userRepository, 
+        IExternalDeeplinkApiService externalDeeplinkApiService, ILogger<IReferralLinkService> logger)
     {
         this.referralLinkRepository = referralLinkRepository;
-        this.userRespository = userRespository;
+        this.userRepository = userRepository;
         this.externalDeeplinkApiService = externalDeeplinkApiService;
+        this.logger = logger;
     }
     
     /// <inheritdoc />
-    public async Task<ReferralLinkDTO> CreateOrGetReferralLink(Guid userId)
+    public async Task<ReferralLinkDTO> CreateOrGetReferralLink(Guid userId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (userId == Guid.Empty)
+        {
+            logger.LogWarning($"User {userId} has not been provided");
+            throw new ArgumentException("User id cannot be empty", nameof(userId));
+        }
+        var user = await userRepository.GetById(userId, cancellationToken);
+        
+        if (user == null)
+        {
+            logger.LogWarning($"User {userId} does not exist");
+            throw new UserNotFoundException(userId);
+        }
+
+        //Let's check if the referral code exists and return it if it does.
+        var referralLink = await referralLinkRepository.GetByUserId(user.Id, cancellationToken);
+        
+        //If it doesn't exist, let's create a new one
+        if (referralLink == null)
+        {
+            var deepLink = await externalDeeplinkApiService.GenerateLink(user.ReferralCode, cancellationToken);
+            logger.LogDebug("Deep link generated. Creating new referral link");
+            var newReferralLink = new ReferralLink
+            {
+                UserId = user.Id,
+                DateCreated = deepLink.DateCreated,
+                ExpirationDate = deepLink.ExpirationDate,
+                ThirdPartyId = deepLink.Id,
+                BaseDeepLink = deepLink.Link
+            };
+            await referralLinkRepository.Create(newReferralLink, cancellationToken);
+            return newReferralLink.ReferralLinkToReferralLinkDto();
+        }
+
+        return referralLink.ReferralLinkToReferralLinkDto();
+
     }
 
     /// <inheritdoc />
-    public async Task<ReferralLinkDTO> ExtendReferralLinkTimeToLive(Guid userId)
+    public async Task<ReferralLinkDTO> ExtendReferralLinkTimeToLive(Guid userId, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
