24046ef64a8f1d49667d34e01c55098e1e006af5
Stephon <stephonb14@gmail.com>
2025-05-22 15:40:24 -0400

    feat: add referralLinkService tests

---
+++ Livefront.Referrals.UnitTests/API/Services/ReferralLinkServiceTests/BaseReferralLinkServiceTestFixture.cs
@@ -0,0 +1,148 @@
+using Livefront.Referrals.API.Services;
+using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Models.DeeplinkApi.Models;
+using Livefront.Referrals.DataAccess.Repositories;
+using Livefront.Referrals.DataAccess.Services;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+
+namespace Livefront.Referrals.UnitTests.API.Services.ReferralLinkServiceTests;
+
+public class BaseReferralLinkServiceTestFixture
+{
+    protected IExternalDeeplinkApiService mockedExternalDeeplinkApiService =
+        Substitute.For<IExternalDeeplinkApiService>();
+
+    protected IReferralLinkRepository mockedReferralLinkRepository = Substitute.For<IReferralLinkRepository>();
+    protected IUserRepository mockedUserRepository = Substitute.For<IUserRepository>();
+    protected ILogger<IReferralLinkService> mockedLogger = Substitute.For<ILogger<IReferralLinkService>>();
+    protected CancellationToken cancellationToken = CancellationToken.None;
+    protected IReferralLinkService referralLinkService;
+    protected void GivenUserRepositoryGetByIdReturnsNull()
+    {
+        mockedUserRepository
+            .GetById(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .ReturnsNull();
+    }
+
+    protected void GivenReferralLinkRepositoryGetByUserIdReturnsNull()
+    {
+        mockedReferralLinkRepository
+            .GetByUserId(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .ReturnsNull();
+    }
+    protected async Task ThenExternalDeeplinkApiServiceShouldBeCalled(int thirdPartyId, DateTime dateCreated,
+        DateTime dateExpires, string link, int numberOfCalls)
+    {
+        await mockedExternalDeeplinkApiService
+            .Received(numberOfCalls)
+            .UpdateLinkTimeToLive(
+                Arg.Is<DeepLink>(dl => dl.Id == thirdPartyId 
+                                       && dl.DateCreated == dateCreated 
+                                       && dl.ExpirationDate == dateExpires
+                                       && dl.Link == link),
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken ));
+    }
+
+    protected async Task ThenReferralLinkRepositoryGetByUserIdShouldBeCalled(Guid userId,  int numberOfCalls)
+    {
+        await mockedReferralLinkRepository
+            .Received(numberOfCalls)
+            .GetByUserId(Arg.Is<Guid>( id => id == userId ),
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken ));
+    }
+
+    protected async Task ThenUserRepositoryGetByIdShouldBeCalled(Guid userId,  int numberOfCalls)
+    {
+        await mockedUserRepository
+            .Received(numberOfCalls)
+            .GetById(Arg.Is<Guid>( id => id == userId ), 
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken ));
+    }
+
+    protected void GivenExternalDeeplinkApiServiceUpdateLinkTimeToLiveReturnsNull()
+    {
+        mockedExternalDeeplinkApiService.UpdateLinkTimeToLive(
+                Arg.Any<DeepLink>(), 
+                Arg.Any<CancellationToken>())
+            .ReturnsNull();
+    }
+
+    protected void GivenReferralLinkRepositoryGetByUserIdReturnsReferralLink(ReferralLink referralLink)
+    {
+        mockedReferralLinkRepository
+            .GetByUserId(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(referralLink);
+    }
+
+    protected void GivenUserRepositoryGetByUserIdReturnsUser(User user)
+    {
+        mockedUserRepository
+            .GetById(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(user);
+    }
+    protected void GivenExternalDeeplinkApiServiceUpdateLinkTimeToLiveReturnsDeeplink(DeepLink updatedDeepLink)
+    {
+        mockedExternalDeeplinkApiService.UpdateLinkTimeToLive(
+                Arg.Any<DeepLink>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(updatedDeepLink);
+    }
+    protected async Task ThenExternalDeeplinkApiServiceUpdateLinkTimeToLiveShouldBeCalled(int thirdPartyId,
+        DateTime dateCreated, DateTime dateExpires, string link, int numberOfCalls)
+    {
+        await mockedExternalDeeplinkApiService
+            .Received(numberOfCalls)
+            .UpdateLinkTimeToLive(
+                Arg.Is<DeepLink>(dl => dl.Id == thirdPartyId 
+                                       && dl.DateCreated == dateCreated 
+                                       && dl.ExpirationDate == dateExpires
+                                       && dl.Link == link),
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken ));
+    }
+
+    protected void GivenReferralLinkRepositoryUpdatExpirationDateReturnsReferralLink(ReferralLink updatedReferralLink)
+    {
+        mockedReferralLinkRepository
+            .UpdateExpirationDate(Arg.Any<Guid>(), 
+                Arg.Any<DateTime>(),
+                Arg.Any<CancellationToken>())
+            .Returns(updatedReferralLink);
+    }
+    
+    
+    protected async Task ThenReferralLinkRepositoryCreateShouldBeCalled(ReferralLink referralLink, int numberOfCalls)
+    {
+        await mockedReferralLinkRepository
+            .Received(numberOfCalls)
+            .Create(
+                Arg.Is<ReferralLink>(submittedReferral =>
+                    submittedReferral.BaseDeepLink == referralLink.BaseDeepLink &&
+                    submittedReferral.ExpirationDate == referralLink.ExpirationDate &&
+                    submittedReferral.ThirdPartyId == referralLink.ThirdPartyId &&
+                    submittedReferral.DateCreated == referralLink.DateCreated &&
+                    submittedReferral.Id == Guid.Empty),
+                cancellationToken);
+    }
+
+    protected void GivenReferralLinkRepositoryCreateReturnsReferralLink(ReferralLink referralLink)
+    {
+        mockedReferralLinkRepository
+            .Create(Arg.Any<ReferralLink>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(referralLink);
+    }
+
+    protected void GivenExternalDeeplinkApiServiceGenerateLinkReturnsDeeplink(DeepLink deepLink)
+    {
+        mockedExternalDeeplinkApiService.GenerateLink(
+                Arg.Any<string>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(deepLink);
+    }
+}
--- Livefront.Referrals.UnitTests/API/Services/ReferralLinkServiceTests/WhenExtendingReferralLinkTimeToLive.cs
+++ Livefront.Referrals.UnitTests/API/Services/ReferralLinkServiceTests/WhenExtendingReferralLinkTimeToLive.cs
@@ -1,6 +1,194 @@
+using Livefront.Referrals.API.Exceptions;
+using Livefront.Referrals.API.Services;
+using Livefront.Referrals.DataAccess.Exceptions;
+using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Models.DeeplinkApi.Models;
+using NSubstitute;
+
 namespace Livefront.Referrals.UnitTests.API.Services.ReferralLinkServiceTests;
 
-public class WhenExtendingReferralLinkTimeToLive
+public class WhenExtendingReferralLinkTimeToLive : BaseReferralLinkServiceTestFixture
 {
-    
+    [SetUp]
+    public void Setup()
+    {
+        referralLinkService = new ReferralLinkService(mockedReferralLinkRepository,
+            mockedUserRepository,
+            mockedExternalDeeplinkApiService,
+            mockedLogger);
+    }
+
+    [Test]
+    public async Task GoldenPath()
+    {
+        //Arrange
+        var userId = Guid.NewGuid();
+        var thirdPartyId = 1003;
+        var link = "https://my-deeplink.com/";
+        var dateCreated = DateTime.UtcNow.AddDays(30);
+        var dateExpires = dateCreated.AddDays(1);
+
+        var user = new User()
+        {
+            Id = userId,
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            ReferralCode = "THISISMYCODE"
+        };
+        var referralLink = new ReferralLink()
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            BaseDeepLink = link,
+            DateCreated = dateCreated,
+            ExpirationDate = dateExpires,
+            ThirdPartyId = thirdPartyId
+        };
+
+        var updatedDeepLink = new DeepLink(
+            thirdPartyId,
+            DateTime.UtcNow,
+            dateExpires.AddDays(31),
+            link);
+
+        var updatedReferralLink = new ReferralLink()
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            BaseDeepLink = link,
+            DateCreated = dateCreated,
+            ExpirationDate = updatedDeepLink.ExpirationDate,
+            ThirdPartyId = thirdPartyId
+        };
+
+        GivenUserRepositoryGetByUserIdReturnsUser(user);
+        GivenReferralLinkRepositoryGetByUserIdReturnsReferralLink(referralLink);
+        GivenExternalDeeplinkApiServiceUpdateLinkTimeToLiveReturnsDeeplink(updatedDeepLink);
+        GivenReferralLinkRepositoryUpdatExpirationDateReturnsReferralLink(updatedReferralLink);
+
+        //Act
+        var returnedReferralLinkDto = await referralLinkService.ExtendReferralLinkTimeToLive(userId, cancellationToken);
+
+        //Assert
+        Assert.That(returnedReferralLinkDto, Is.Not.Null);
+        Assert.That(returnedReferralLinkDto.ReferralLink, Is.EqualTo(updatedReferralLink.BaseDeepLink));
+        Assert.That(returnedReferralLinkDto.ExpirationDate, Is.EqualTo(updatedReferralLink.ExpirationDate));
+
+        await ThenUserRepositoryGetByIdShouldBeCalled(userId, 1);
+        await ThenReferralLinkRepositoryGetByUserIdShouldBeCalled(userId, 1);
+        await ThenExternalDeeplinkApiServiceUpdateLinkTimeToLiveShouldBeCalled(thirdPartyId, dateCreated, dateExpires,
+            link, 1);
+        await ThenReferralLinkRepositoryUpdateExpirationDateShouldBeCalled(user, updatedDeepLink, 1);
+    }
+
+    private async Task ThenReferralLinkRepositoryUpdateExpirationDateShouldBeCalled(User user, DeepLink updatedDeepLink,
+        int numberOfCalls)
+    {
+        await mockedReferralLinkRepository
+            .Received(numberOfCalls)
+            .UpdateExpirationDate(
+                Arg.Is<Guid>(id => id == user.Id),
+                Arg.Is<DateTime>(date => date == updatedDeepLink.ExpirationDate),
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken));
+    }
+
+
+    [Test]
+    public async Task GivenUserDoesNotExist_ThenThrowUserNotFoundException()
+    {
+        //Arrange
+        var userId = Guid.NewGuid();
+        GivenUserRepositoryGetByIdReturnsNull();
+        //Act/Assert
+        Assert.ThrowsAsync<UserNotFoundException>(async () =>
+            await referralLinkService.ExtendReferralLinkTimeToLive(userId, cancellationToken));
+        await ThenUserRepositoryGetByIdShouldBeCalled(userId, 1);
+
+    }
+
+
+    [Test]
+    public void GivenUserIdIsEmpty_ThenThrowArgumentException()
+    {
+        //Arrange
+        var userId = Guid.Empty;
+
+        //Act/Assert
+        var exception = Assert.ThrowsAsync<ArgumentException>(async () =>
+            await referralLinkService.ExtendReferralLinkTimeToLive(userId, cancellationToken));
+        Assert.That(exception.ParamName, Is.EqualTo("userId"));
+    }
+
+    [Test]
+    public async Task GivenReferralLinkIsNull_ThenThrowReferralLinkNotFoundException()
+    {
+        //Arrange
+        var userId = Guid.NewGuid();
+
+        var user = new User()
+        {
+            Id = userId,
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            ReferralCode = "THISISMYCODE"
+        };
+
+        GivenUserRepositoryGetByUserIdReturnsUser(user);
+        GivenReferralLinkRepositoryGetByUserIdReturnsNull();
+
+        //Act/Assert
+        var exception = Assert.ThrowsAsync<ReferralLinkNotFoundException>(async () =>
+            await referralLinkService.ExtendReferralLinkTimeToLive(userId, cancellationToken));
+        Assert.That(exception.UserId, Is.EqualTo(userId));
+
+        await ThenUserRepositoryGetByIdShouldBeCalled(userId, 1);
+        await ThenReferralLinkRepositoryGetByUserIdShouldBeCalled(userId, 1);
+
+    }
+
+
+    [Test]
+    public async Task GivenExternalDeeplinkIsNull_ThenThrowExternalApiServiceException()
+    {
+        //Arrange
+        var userId = Guid.NewGuid();
+        var thirdPartyId = 1003;
+        var link = "https://my-deeplink.com/";
+        var dateCreated = DateTime.UtcNow.AddDays(30);
+        var dateExpires = dateCreated.AddDays(1);
+
+        var user = new User()
+        {
+            Id = userId,
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            ReferralCode = "THISISMYCODE"
+        };
+
+        var referralLink = new ReferralLink()
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            BaseDeepLink = link,
+            DateCreated = dateCreated,
+            ExpirationDate = dateExpires,
+            ThirdPartyId = thirdPartyId
+        };
+
+        GivenUserRepositoryGetByUserIdReturnsUser(user);
+        GivenReferralLinkRepositoryGetByUserIdReturnsReferralLink(referralLink);
+        GivenExternalDeeplinkApiServiceUpdateLinkTimeToLiveReturnsNull();
+
+        //Act/Assert
+        Assert.ThrowsAsync<ExternalApiServiceException>(async () =>
+            await referralLinkService.ExtendReferralLinkTimeToLive(userId, cancellationToken));
+
+        await ThenUserRepositoryGetByIdShouldBeCalled(userId, 1);
+        await ThenReferralLinkRepositoryGetByUserIdShouldBeCalled(userId, 1);
+        await ThenExternalDeeplinkApiServiceShouldBeCalled(thirdPartyId, dateCreated, dateExpires, link, 1);
+
+    }
 }
