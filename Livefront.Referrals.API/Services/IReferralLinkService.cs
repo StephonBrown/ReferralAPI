9ed7b39a06ccaa6ee0374246c05bbabe8186a931
Stephon Brown <stephonb14@gmail.com>
2025-05-23 00:53:06 -0400

    fix: split create and retrieve logic

--- Livefront.Referrals.API/Services/IReferralLinkService.cs
+++ Livefront.Referrals.API/Services/IReferralLinkService.cs
@@ -3,23 +3,31 @@ using Livefront.Referrals.API.Models;
 namespace Livefront.Referrals.API.Services;
 
 /// <summary>
-/// A service for managing User ReferralLinks 
+/// A service for managing referral links
 /// </summary>
 public interface IReferralLinkService
 {
     /// <summary>
-    /// Creates a new referral link for a user or retrieves an existing one if it already exists.
+    /// Creates a new referral link for a user or returns an existing one if it already exists.
     /// </summary>
     /// <param name="userId">The unique id of the user.</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation if needed or on timeout.</param> 
     /// <returns>A <see cref="Task"/> that represents the async operation, containing the <see cref="ReferralLinkDTO"/>.</returns>
-    Task<ReferralLinkDTO?> CreateOrGetReferralLink(Guid userId, CancellationToken cancellationToken);
-
+    Task<ReferralLinkDTO?> CreateReferralLink(Guid userId, CancellationToken cancellationToken);
+    
     /// <summary>
-    /// Extends the time-to-live (TTL) of a referral link for a specific user.
+    /// Retrieves the referral link for a specific user if it exists, or returns null if it doesn't.
+    /// </summary>
+    /// <param name="userId">The user identifier</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation if needed or on timeout.</param>
+    /// <returns>A <see cref="Task"/> that represents the async operation, containing the <see cref="ReferralLinkDTO"/>.</returns>
+    Task<ReferralLinkDTO?> GetReferralLink(Guid userId, CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// Extends the time-to-live of a referral link for a specific user.
     /// </summary>
     /// <param name="userId">The unique id of the user.</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation if needed or on timeout.</param>
     /// <returns>A <see cref="Task"/> that represents the async operation, containing the updated <see cref="ReferralLinkDTO"/>.</returns>
     Task<ReferralLinkDTO?> ExtendReferralLinkTimeToLive(Guid userId, CancellationToken cancellationToken);
 }
--- Livefront.Referrals.API/Services/ReferralLinkService.cs
+++ Livefront.Referrals.API/Services/ReferralLinkService.cs
@@ -14,34 +14,24 @@ public class ReferralLinkService : IReferralLinkService
     private readonly IExternalDeeplinkApiService externalDeeplinkApiService;
     private readonly IUserRepository userRepository;
     private readonly ILogger<IReferralLinkService> logger;
-    
+    private IReferralLinkService _referralLinkServiceImplementation;
+
     public ReferralLinkService(IReferralLinkRepository referralLinkRepository, 
         IUserRepository userRepository, 
         IExternalDeeplinkApiService externalDeeplinkApiService, ILogger<IReferralLinkService> logger)
-    {
+    {       
         this.referralLinkRepository = referralLinkRepository;
         this.userRepository = userRepository;
         this.externalDeeplinkApiService = externalDeeplinkApiService;
         this.logger = logger;
     }
-    
     /// <inheritdoc />
-    public async Task<ReferralLinkDTO?> CreateOrGetReferralLink(Guid userId, CancellationToken cancellationToken)
+    public async Task<ReferralLinkDTO?> CreateReferralLink(Guid userId, CancellationToken cancellationToken)
     {
-        if (userId == Guid.Empty)
-        {
-            logger.LogWarning($"User {userId} has not been provided");
-            throw new ArgumentException("User id cannot be empty", nameof(userId));
-        }
-        var user = await userRepository.GetById(userId, cancellationToken);
-        
-        if (user == null)
-        {
-            logger.LogWarning($"User {userId} does not exist");
-            throw new UserNotFoundException(userId);
-        }
+        var user = await ValidateUserIdAndReturnUser(userId, cancellationToken);
 
         var referralLink = await referralLinkRepository.GetByUserId(user.Id, cancellationToken);
+        
         if (referralLink == null)
         {
             var deepLink = await externalDeeplinkApiService.GenerateLink(user.ReferralCode, cancellationToken);
@@ -50,7 +40,9 @@ public class ReferralLinkService : IReferralLinkService
                 logger.LogWarning("Generated deeplink was not returned");
                 throw new ExternalApiServiceException("Error while creating deep link with external service");
             }
+            
             logger.LogDebug("Deep link generated. Creating new referral link");
+            
             var newReferralLink = new ReferralLink
             {
                 UserId = user.Id,
@@ -59,12 +51,23 @@ public class ReferralLinkService : IReferralLinkService
                 ThirdPartyId = deepLink.Id,
                 BaseDeepLink = deepLink.Link
             };
+            
             await referralLinkRepository.Create(newReferralLink, cancellationToken);
+            
+            logger.LogDebug("Referral link created for user {UserId}", userId);
             return newReferralLink.ToReferralLinkDto();
         }
 
         return referralLink.ToReferralLinkDto();
+    }
+    
 
+    public async Task<ReferralLinkDTO?> GetReferralLink(Guid userId, CancellationToken cancellationToken)
+    {
+        var user = await ValidateUserIdAndReturnUser(userId, cancellationToken);
+
+        var referralLink = await referralLinkRepository.GetByUserId(user!.Id, cancellationToken);
+        return referralLink.ToReferralLinkDto();
     }
 
     /// <inheritdoc />
@@ -109,5 +112,24 @@ public class ReferralLinkService : IReferralLinkService
             throw new DataPersistenceException("Error while updating referral link");
         }
         return updatedReferralLink.ToReferralLinkDto(); 
+    }
+    
+    private async Task<User?> ValidateUserIdAndReturnUser(Guid userId, CancellationToken cancellationToken)
+    {
+        if (userId == Guid.Empty)
+        {
+            logger.LogWarning($"User {userId} has not been provided");
+            throw new ArgumentException("User id cannot be empty", nameof(userId));
+        }
+
+        var user = await userRepository.GetById(userId, cancellationToken);
+
+        if (user == null)
+        {
+            logger.LogWarning($"User {userId} does not exist");
+            throw new UserNotFoundException(userId);
+        }
+
+        return user;
     }
 }
