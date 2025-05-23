ae3c791b4780fe29a4d59e8e5437623398297d25
Stephon Brown <stephonb14@gmail.com>
2025-05-23 15:31:17 -0400

    fix: update exceptions for property specific instantiating

--- Livefront.Referrals.API/Exceptions/UserNotFoundException.cs
+++ Livefront.Referrals.API/Exceptions/UserNotFoundException.cs
@@ -1,6 +1,18 @@
 namespace Livefront.Referrals.API.Exceptions;
 
-public class UserNotFoundException(Guid userId) : Exception($"User {userId} was not found")
+public class UserNotFoundException : Exception
 {
-    public Guid UserId { get; private set; } = userId;
+    public Guid UserId { get; private set; }
+    public string ReferralCode { get; private set; }
+    
+    public UserNotFoundException(Guid userId) : base($"User {userId} was not found")
+    {
+        UserId = userId;
+    }
+    public UserNotFoundException(string referralCode) : base($"User with referral code {referralCode} was not found")
+    {
+        ReferralCode = referralCode;
+    }
+
+
 }
