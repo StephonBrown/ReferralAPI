645e88c3d8d3097f39867678eb9f3f6b97aca28d
Stephon Brown <stephonb14@gmail.com>
2025-05-23 11:39:22 -0400

    feat: add baseservicetestfixture to DRY out test properties

---
+++ Livefront.Referrals.UnitTests/API/Services/BaseServiceTestFixture.cs
@@ -0,0 +1,36 @@
+using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Repositories;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+
+namespace Livefront.Referrals.UnitTests.API.Services;
+
+public class BaseServiceTestFixture
+{
+    protected IUserRepository mockedUserRepository = Substitute.For<IUserRepository>();
+    protected CancellationToken cancellationToken = CancellationToken.None;
+
+    protected void GivenUserRepositoryGetByIdReturnsNull()
+    {
+        mockedUserRepository
+            .GetById(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .ReturnsNull();
+    }
+    
+    protected async Task ThenUserRepositoryGetByIdShouldBeCalled(Guid userId,  int numberOfCalls)
+    {
+        await mockedUserRepository
+            .Received(numberOfCalls)
+            .GetById(Arg.Is<Guid>( id => id == userId ), 
+                Arg.Is<CancellationToken>(ct => ct == cancellationToken ));
+    }
+    
+    protected void GivenUserRepositoryGetByUserIdReturnsUser(User user)
+    {
+        mockedUserRepository
+            .GetById(Arg.Any<Guid>(), 
+                Arg.Any<CancellationToken>())
+            .Returns(user);
+    }
+}
