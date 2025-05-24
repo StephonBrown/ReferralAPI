c08f6ed71e8788d78a987bfe84bad29da7bfdcc1
Stephon Brown <stephonb14@gmail.com>
2025-05-24 03:20:33 -0400

    fix: cleanup referralscontext

--- Livefront.Referrals.DataAccess/ReferralsContext.cs
+++ Livefront.Referrals.DataAccess/ReferralsContext.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Livefront.Referrals.DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,12 +6,18 @@ namespace Livefront.Referrals.DataAccess;
 
 public class ReferralsContext : DbContext
 {
+    public string DbPath { get; init; }
     public DbSet<Referral> Referrals { get; set; }
     public DbSet<ReferralLink> ReferralLinks { get; set; }
     public DbSet<User> Users { get; set; }
     
-    public ReferralsContext(DbContextOptions options): base(options)
+
+    public ReferralsContext(DbContextOptions options) : base(options)
     {
-        
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+    
 }
