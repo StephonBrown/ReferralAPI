2bd3fe71c40b65bcc72be5986033bd1ca080cca2
Stephon Brown <stephonb14@gmail.com>
2025-05-24 03:19:31 -0400

    feat: add authentication

---
+++ Livefront.Referrals.API/Controllers/AuthTestController.cs
@@ -0,0 +1,86 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Protocol;
+
+namespace Livefront.Referrals.API.Controllers;
+/// <summary>
+/// This is merely a test controller to test provide developers and testers with a way to
+/// access the API without having to go through the full authentication process.
+/// This is not intended, or recommended for production use.
+/// The test user that is returned is seeded into the database for test usage.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class AuthTestController : ControllerBase
+{
+    private readonly ILogger<AuthTestController> logger;
+    private readonly IUserRepository userRepository;
+    private readonly IConfiguration configuration;
+
+    public AuthTestController(IUserRepository userRepository, IConfiguration configuration, ILogger<AuthTestController> logger)
+    {
+        this.userRepository = userRepository;
+        this.configuration = configuration;
+        this.logger = logger;
+    }
+    
+    [AllowAnonymous]
+    [HttpPost]
+    [Route("get-bearer-token")]
+    public async Task<IActionResult> GetJWTBearerToken([FromBody]Secret secret, CancellationToken cancellationToken)
+    {
+        
+        logger.LogInformation("Auth test endpoint called");
+        if(secret.SecretCode == "TEST")
+        {
+            var testUser = await userRepository.GetUserByReferralCode("TESTCODE", cancellationToken);
+            var token = GenerateAccessToken(testUser!);
+            return Ok(new { AccessToken = new JwtSecurityTokenHandler().WriteToken(token)});
+        }
+        return BadRequest("Invalid secret code. Please provide the correct secret code.");
+    }
+    
+    /// <summary>
+    /// This method generates a JWT access token for the a test user
+    /// </summary>
+    /// <param name="user">the user who will use the token</param>
+    /// <returns>the generated JWT token</returns>
+    private JwtSecurityToken GenerateAccessToken(User user)
+    {
+        // Create user claims
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.FirstName),
+            new Claim(ClaimTypes.Sid, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+
+        // Create a JWT
+        var token = new JwtSecurityToken(
+            issuer: configuration["JwtSettings:Issuer"],
+            audience: configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1), //Expires in an hour
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]!)),
+                SecurityAlgorithms.HmacSha256)
+        );
+
+        return token;
+    }
+    
+    // This is necessary to be able to deserialize the request body
+    public class Secret(string SecretCode)
+    {
+        [JsonPropertyName("secret_code")]
+        public string SecretCode { get; set; } = SecretCode;
+    }
+}
+
+    
--- Livefront.Referrals.API/Program.cs
+++ Livefront.Referrals.API/Program.cs
@@ -1,5 +1,14 @@
+using System.Text;
+using Livefront.BusinessLogic.Services;
 using Livefront.Referrals.API.Configuration;
+using Livefront.Referrals.API.Services;
+using Livefront.Referrals.DataAccess;
+using Livefront.Referrals.DataAccess.Models;
+using Livefront.Referrals.DataAccess.Repositories;
 using Livefront.Referrals.DataAccess.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 namespace Livefront.Referrals.API;
@@ -13,7 +22,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddControllers();
         AddNameHttpClients(builder);
-        
+        ConfigureServices(builder.Services);
         // Configure logging using Serilog Sinks for both the console and a local file
         // Note: We can explore other sinks in the future for visualization
         // This gets the configuration from app-settings.json
@@ -21,13 +30,30 @@ public class Program
             loggerConfig.ReadFrom.Configuration(context.Configuration)
         );
         builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-        var app = builder.Build();
         
+        //This sets up JWT configuration for our authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!))
+                };
+            });
+        
+        var app = builder.Build();
+        CreateNewDatase(app.Services);
         app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
         app.UseSerilogRequestLogging();
-
+        app.UseCors();
         app.MapControllers();
 
         app.Run();
@@ -48,5 +74,71 @@ public class Program
             client.DefaultRequestHeaders.Add("ApiToken", deeplinkApiConfig.ApiToken);
             client.DefaultRequestHeaders.Add("ApiTokenSecret", deeplinkApiConfig.ApiToken);
         });
+    }
+    
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddDbContext<ReferralsContext>(options =>
+        {
+            // Seeding for both sync and async is recommended since some EF tooling uses sync still
+            options.UseSqlite("Data Source=Referrals.db");
+        });
+        services.AddScoped<IReferralRepository, ReferralRepository>();
+        services.AddScoped<IReferralLinkRepository, ReferralLinkRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IReferralService, ReferralService>();
+        services.AddScoped<IReferralLinkService, ReferralLinkService>();
+        services.AddScoped<IExternalDeeplinkApiService, MockDeeplinkApiService>();
+    }
+    
+    private static void Seed(DbContext context)
+    {
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        context
+            .Set<User>()
+            .AddRange(GetUsers());
+        context.SaveChanges();
+    }
+
+
+    private static IEnumerable<User> GetUsers()
+    {
+        return new[]
+        {
+            new User
+            {
+                FirstName = "Test",
+                LastName = "User",
+                Email = "test@email.com",
+                ReferralCode = "TESTCODE"
+            },
+            new User
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "John@email.com",
+                ReferralCode = "JOHNCODE"
+            },
+            new User
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "Jane@email.com",
+                ReferralCode = "JANECODE"
+            }
+        };
+    }
+
+    private static void CreateNewDatase(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ReferralsContext>();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            Seed(context);
+        }
     }
 }
