e0cf0069b916f6e83bbea382f9ca0e763d05104f
Stephon Brown <stephonb14@gmail.com>
2025-05-25 12:19:08 -0400

    feat: add functional test base

--- test/Livefront.Referrals.FunctionalTests/API/BaseControllerTestFixture.cs
+++ test/Livefront.Referrals.FunctionalTests/API/BaseControllerTestFixture.cs
@@ -1,6 +1,21 @@
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using Livefront.Referrals.API.Models;
+
 namespace Livefront.Referrals.FunctionalTests.API;
 
 public class BaseControllerTestFixture
 {
+    protected TestWebApplicationFactory? webApplicationFactory;
     
+    protected async Task<AccessToken?> GetAuthToken(HttpClient client)
+    {
+        var secret = new RequestSecret("TEST");
+        var authToken = await client.PostAsync("/api/authtest/get-bearer-token",
+            new StringContent(JsonSerializer.Serialize(secret),
+                Encoding.UTF8,
+                "application/json"));
+        return await authToken.Content.ReadFromJsonAsync<AccessToken>();
+    }
 }
