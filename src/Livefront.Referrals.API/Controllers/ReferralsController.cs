bb5ab418be4af0ecc7d7a6bdfabf55ed685f6814
Stephon Brown <stephonb14@gmail.com>
2025-05-25 12:20:12 -0400

    feat: implement golden path

--- src/Livefront.Referrals.API/Controllers/ReferralsController.cs
+++ src/Livefront.Referrals.API/Controllers/ReferralsController.cs
@@ -1,4 +1,6 @@
 using Livefront.Referrals.API.Models;
+using Livefront.Referrals.API.Services;
+using Livefront.Referrals.DataAccess.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +11,12 @@ namespace Livefront.Referrals.API.Controllers
     public class ReferralsController : ControllerBase
     {
         private readonly ILogger<ReferralsController> logger;
+        private readonly IReferralService referralService;
 
-        public ReferralsController(ILogger<ReferralsController> logger)
+        public ReferralsController(ILogger<ReferralsController> logger, IReferralService referralService)
         {
             this.logger = logger;
+            this.referralService = referralService;
         }
         
         [Authorize]
@@ -25,11 +29,34 @@ namespace Livefront.Referrals.API.Controllers
         
         [Authorize]
         [HttpPost]
-        public IActionResult CompleteReferral([FromBody]CreateReferralRequest createReferralRequest)
+        public async Task<IActionResult> CompleteReferral([FromBody]CreateReferralRequest? createReferralRequest, CancellationToken cancellationToken)
         {
-            logger.LogInformation("CreateReferral called");
-            return Ok(createReferralRequest);
+            logger.LogDebug("CompleteReferral called");
+            ValidateCreateReferralRequest(createReferralRequest);
+            var referralDto = await referralService.CreateReferral(createReferralRequest!.RefereeId, createReferralRequest.ReferralCode, cancellationToken);
+            return Ok(referralDto);
         }
-        
+
+        private void ValidateCreateReferralRequest(CreateReferralRequest? createReferralRequest)
+        {
+            if (createReferralRequest == null)
+            {
+                logger.LogWarning("CreateReferralRequest is null");
+                throw new ArgumentNullException(nameof(createReferralRequest), "CreateReferralRequest cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(createReferralRequest.ReferralCode))
+            {
+                logger.LogWarning("Referral code is null or empty");
+                throw new ArgumentException("Referral code cannot be null or empty",
+                    nameof(createReferralRequest.ReferralCode));
+            }
+
+            if (createReferralRequest.RefereeId == Guid.Empty)
+            {
+                logger.LogWarning("Referee user ID is empty");
+                throw new ArgumentException("Referee user ID cannot be empty", nameof(createReferralRequest.RefereeId));
+            }
+        }
     }
 }
