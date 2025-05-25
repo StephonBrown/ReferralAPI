f8bb79527135380486d3031e65dc586576eefe36
Stephon Brown <stephonb14@gmail.com>
2025-05-25 12:15:33 -0400

    feat: add referral extension tests and updates

--- src/Livefront.BusinessLogic/Models/ReferralDTO.cs
+++ src/Livefront.BusinessLogic/Models/ReferralDTO.cs
@@ -5,6 +5,8 @@ namespace Livefront.BusinessLogic.Models;
 
 public class ReferralDTO
 {
+    [JsonPropertyName("referral_id")] public Guid ReferralId { get; init; }
+    [JsonPropertyName("user_id")] public Guid UserId { get; init; }
     [JsonPropertyName("first_name")] public string FirstName { get; init; }
     [JsonPropertyName("last_name")] public string LastName { get; init; }
     [JsonPropertyName("status")]public ReferralStatus Status { get; init; }
