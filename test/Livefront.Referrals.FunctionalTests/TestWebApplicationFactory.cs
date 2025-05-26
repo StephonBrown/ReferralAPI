b5631ef50bdd5548caf0b2d7ead27ee3e9eb9064
Stephon Brown <stephonb14@gmail.com>
2025-05-26 15:13:17 -0400

    fix: update test factory

--- test/Livefront.Referrals.FunctionalTests/TestWebApplicationFactory.cs
+++ test/Livefront.Referrals.FunctionalTests/TestWebApplicationFactory.cs
@@ -1,7 +1,15 @@
 using Livefront.Referrals.API;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Livefront.Referrals.FunctionalTests;
 
 public class TestWebApplicationFactory : WebApplicationFactory<Program>
-{ }
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Make sure the application runs in development mode
+        builder.UseEnvironment("Development");
+        base.ConfigureWebHost(builder);
+    }
+}
