b513fb79bf9ffe6071dee83ea0221231963c1c88
Stephon Brown <stephonb14@gmail.com>
2025-05-26 05:25:55 -0400

    fix: implement referral links controller

--- src/Livefront.Referrals.API/Controllers/ReferralLinksController.cs
+++ src/Livefront.Referrals.API/Controllers/ReferralLinksController.cs
@@ -1,5 +1,7 @@
+using System.Security.Claims;
+using Livefront.Referrals.API.Services;
+using Livefront.Referrals.DataAccess.Repositories;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Livefront.Referrals.API.Controllers
@@ -9,26 +11,61 @@ namespace Livefront.Referrals.API.Controllers
     public class ReferralLinksController : ControllerBase
     {
         private readonly ILogger<ReferralLinksController> logger;
+        private readonly IUserRepository userRepository;
+        private readonly IReferralLinkService referralLinkService;
 
-        public ReferralLinksController(ILogger<ReferralLinksController> logger)
+        public ReferralLinksController(ILogger<ReferralLinksController> logger, IUserRepository userRepository, IReferralLinkService referralLinkService)
         {
             this.logger = logger;
-        }
-    
-        [Authorize]
-        [HttpPost]
-        public IActionResult CreateReferralLink(Guid id)
-        {
-            logger.LogInformation("CreateReferral called");
-            return Ok();
+            this.userRepository = userRepository;
+            this.referralLinkService = referralLinkService;
         }
         
         [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetReferralLink(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetReferralLink called");
+            var userId = User.FindFirstValue(ClaimTypes.Sid);
+            if (string.IsNullOrEmpty(userId))
+            {
+                logger.LogWarning("User ID not found in claims");
+                throw new UnauthorizedAccessException("User ID not found in claims. Ensure the user is authenticated.");
+            }
+            var referralLink = await referralLinkService.GetReferralLink(Guid.Parse(userId), cancellationToken);
+            return Ok(referralLink);
+        }
+        
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateReferralLink(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("CreateReferral called");
+            var userId = User.FindFirstValue(ClaimTypes.Sid);
+            if (string.IsNullOrEmpty(userId))
+            {
+                logger.LogWarning("User ID not found in claims");
+                throw new UnauthorizedAccessException("User ID not found in claims. Ensure the user is authenticated.");
+            }
+            
+            var referralLink = await referralLinkService.CreateReferralLink(Guid.Parse(userId), cancellationToken);
+            return Ok(referralLink);
+        }
+
+        
+        [Authorize]
         [HttpPut]
-        public IActionResult UpdateExpirationDate(Guid id)
+        public async Task<IActionResult> UpdateExpirationDate(CancellationToken cancellationToken)
         {
             logger.LogInformation("UpdateReferral called");
-            return Ok();
+            var userId = User.FindFirstValue(ClaimTypes.Sid);
+            if (string.IsNullOrEmpty(userId))
+            {
+                logger.LogWarning("User ID not found in claims");
+                throw new UnauthorizedAccessException("User ID not found in claims. Ensure the user is authenticated.");
+            }
+            var referralLink = await referralLinkService.ExtendReferralLinkTimeToLive(Guid.Parse(userId), cancellationToken);
+            return Ok(referralLink);
         }
     }
 }
